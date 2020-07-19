d902dcab7c19a8828a3b0da85f0129a470b3a646
bet365yasuo <bet365yasuo@gmail.com>
2020-07-19 20:52:22 +0700

    change all

---
+++ TeduShop.Data/TeduShopDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduShop.Data
+{
+    public class TeduShopDbContext:DbContext
+    {
+        public TeduShopDbContext() : base("")
+        {
+
+        }
+    }
+}
