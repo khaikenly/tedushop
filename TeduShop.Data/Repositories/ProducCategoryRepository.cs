a98716f76ecb4d6813f412e862597d0ca82fa6e5
bet365yasuo <bet365yasuo@gmail.com>
2020-07-21 15:41:43 +0700

    change data

---
+++ TeduShop.Data/Repositories/ProducCategoryRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IProducCategoryRepository
+    {
+        IEnumerable<ProductCategory> GetByAlias(string alias);
+    }
+
+    public class ProductCategoryRepository: RepositoryBase<ProductCategory>,IProducCategoryRepository
+    {
+        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory) {
+
+        }
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
+        {
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
+        }
+    }
+}
